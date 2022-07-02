9152170eeffbb34f5cc3c43cb60fdb27b40321f1
sneha <snehacshaji202@gmail.com>
2022-07-02 21:53:47 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import Logo from "./assets/images/logo.png"
+import Logo from "./assets/images/logo.png";
+import Blur from './assets/images/sphere.gif'
 
 function App() {
-  
   return (
     <div className="App">
       <nav>
@@ -10,11 +10,25 @@ function App() {
         <button className="button">Get Started</button>
       </nav>
 
-      <div className="content">
-        <h1>The ultimate <br/>pothole detector</h1>
-        <p>We target a future of roads with no potholes. Our team resonate presents<br/>
-        the ultimate pothole detector. Reduce Potholes, reduce accidents.</p>
-        <button className="button">Try now</button>
+      <div className="whole">
+        <div className="content">
+          <h1>
+            The ultimate <br />
+            pothole detector
+          </h1>
+          <p>
+            We target a future of roads with no potholes. Our team resonate
+            presents
+            <br />
+            the ultimate pothole detector. Reduce Potholes, reduce accidents.
+          </p>
+          <button className="button">Try now</button>
+        </div>
+
+        <div className="image">
+          <img src={Blur} className="blur" alt="blur" />
+        </div>
+
       </div>
     </div>
   );
