8d30d4ea75f37c9427981750eec6f64a9b483a4e
sneha <snehacshaji202@gmail.com>
2022-07-02 21:30:43 +0530

    Content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import Logo from "./assets/images/logo.png"
 
 function App() {
   
   return (
     <div className="App">
-      Hi
+      <nav>
+        <img className="logo" src={Logo} alt="Logo" />
+        <button className="button">Get Started</button>
+      </nav>
+
+      <div className="content">
+        <h1>The ultimate <br/>pothole detector</h1>
+        <p>We target a future of roads with no potholes. Our team resonate presents<br/>
+        the ultimate pothole detector. Reduce Potholes, reduce accidents.</p>
+        <button className="button">Try now</button>
+      </div>
     </div>
   );
 }
